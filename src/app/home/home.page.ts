683fef526d48ebc6f17f20275f55196fb928210a
Marcin Dlubis <marcindl@hotmail.com>
2020-03-05 13:26:34 +0100

    parameters

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+  data: any;
+  now: any;
+  public date: string = new Date().toISOString();
+  // myDate: String = new Date().toISOString();
+  myDate: Date;
+  constructor() { }
+
+  ngOnInit() {
+    fetch('./assets/data/data.ts').then(res => res.json())
+      .then(json => {
+        this.data = json;
+
+        for (let item of this.data) {
+          console.log(item.company)
+        }
+      });
+
+    setInterval(() => {         //replaced function() by ()=>
+      this.myDate = new Date();
+      console.log(this.myDate); // just testing if it is working
+    }, 1000);
+
+  }
+}
---
+++ src/assets/data/data.ts
@@ -0,0 +1,21 @@
+[
+    { "temperature": 12 },
+    { "heartAction": "prawidłowa" },
+    { "energyChoosen": "atomowa" },
+    { "so2": 16 },
+    { "charging": "kabel" },
+    { "etc02": "w normie" },
+    { "discharge": "atmosferyczne" },
+    { "pressure": "120/50" },
+    { "ekg12": 45 },
+    { "medicalStudies": "histopatologia" },
+    { "breathNumber": 56 },
+    { "synchronization": "włączona" },
+
+    { "name": "Ram", "company": "Google" },
+    { "name": "Bob", "company": "EMC" },
+    { "name": "Ram", "company": "Google" },
+    { "name": "Bob", "company": "EMC" },
+    { "name": "Ram", "company": "Google" },
+    { "name": "Bob", "company": [12, 1, 4, 3, 42, 34, 3, 543, 5, 24, 12, 3412, 342134] }
+]  
